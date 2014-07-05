1e1cf3feded4f2dff198bb7fed87604b669bac7e
Elliot Block <elliot@deck36.net>
2014-07-04 19:47:52 -0700

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name = "Labrador",
+    version = "0.1.0",
+    packages = find_packages(),
+    install_requires = [
+        'pyyaml',
+        'jprops',
+        'requests'],
+    author = "Elliot Block",
+    author_email = "elliot@deck36.net",
+    description = "Configuration retrieval",
+    url = "https://github.com/elliot42/labrador"
+)
