50c2b5abc8f9ce564386ccce455c015504fdd4e3
Elliot Block <elliot@deck36.net>
2014-07-05 09:01:06 -0700

    Bump minor version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "Labrador",
-    version = "0.2.2",
+    version = "0.3.0",
     packages = find_packages(),
     install_requires = [
         'pyyaml',
