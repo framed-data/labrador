8f2b0685c55b9dc8256018a734481edbd98a6fa2
Elliot Block <elliot@deck36.net>
2014-07-04 23:04:03 -0700

    Stringify all values when outputting .properties
    .properties format needs all values to be strings;
    convert everything to a string via `str()` to do so.

--- labrador/output/properties.py
+++ labrador/output/properties.py
@@ -3,5 +3,6 @@ import StringIO
 
 def dumps(d):
     out = StringIO.StringIO()
-    jprops.store_properties(out, d)
+    stringified_d = { k:str(v) for k, v in d.iteritems() }
+    jprops.store_properties(out, stringified_d)
     return out.getvalue()
